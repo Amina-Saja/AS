d1f131911f392e8102dc63366b38059222518853
Amina-Saja <sajaabduljaleel@gmail.com>
2022-02-25 16:28:34 +0530

    message

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'in.html')
+
+def sign(request):
+    return render(request,'signup.html')
+
 # Create your views here.
